f4c44a74223c6b1d408439b83cffe1e0e384e147
chris.xue <xuekun.xk@alibaba-inc.com>
2015-06-30 22:31:35 +0800

    update speak api

--- node/routes/answer.js
+++ node/routes/answer.js
@@ -43,76 +43,69 @@ router.get('/session', function (req, res, next) {
 });
 
 router.post('/speak', function (req, res, next) {
-    //生成multiparty对象，并配置下载目标路径
-    var form = new multiparty.Form({uploadDir: './public/images/'});
-    //下载后处理
-    form.parse(req, function (err, fields, files) {
-        var filesTmp = JSON.stringify(files, null, 2);
-        var session_id, sick_id, doctor_id, name, id, content, title, type;
-        try {
-            session_id = fields['session_id'];
-            sick_id = fields['sick_id'];
-            doctor_id = fields['doctor_id'];
-            name = fields['name'];
-            content = fields['content'];
-            title = fields['title'];
-            id = sick_id || doctor_id;
-            type = sick_id ? "sick" : "doctor";
-        } catch (e) {
-            console.error(e);
-            err = e;
-        }
-        if (err) {
-            console.log('parse image error: ' + err);
-            res.redirect(source_url[type] + "?err=1");
-        } else {
-            console.log('parse files: ' + filesTmp);
-            var pic = [];
-            for (var i = 0; i < files.inputFile.length; i++) {
-                pic[i] = files.inputFile[i].path;
-            }
+    var session_id, sick_id, doctor_id, name, id, content, title, type, err, pic;
+    try {
+        session_id = req.param('session_id');
+        sick_id = req.param('sick_id');
+        doctor_id = req.param('doctor_id');
+        name = req.param('name');
+        content = req.param('content');
+        title = req.param('title');
+        pic = req.param('pics');
+        id = sick_id || doctor_id;
+        type = sick_id ? "sick" : "doctor";
+    } catch (e) {
+        console.error(e);
+        err = e;
+    }
+    if (err) {
+        console.log('parse image error: ' + err);
+        res.redirect(source_url[type] + "?err=1");
+    } else {
 
-            if (!session_id) {
-                var msg = {
-                    day: new Date(),
-                    title: title,
-                    content: content,
-                    speaker: name,
-                    speaker_id: id
-                };
-                req.models.message_session.create({
-                    day: new Date(),
-                    sick_id: id,
-                    doctor_id: doctor_id,
-                    message: msg
-                }, function (err, item) {
-                    msg.session_id = item.id;
-                    req.models.message.create(msg, function (err) {
-                        if (err) {
-                            res.redirect(source_url[type] + "?err=2");
-                        } else {
-                            res.redirect(target_url[type]);
-                        }
-                    });
-                });
-            } else {
-                req.models.message.create({
-                    session_id: session_id,
-                    day: new Date(),
-                    title: title,
-                    content: content,
-                    speaker: name,
-                    speaker_id: id
-                }, function (err, item) {
+        if (!session_id) {
+            var msg = {
+                day: new Date(),
+                title: title,
+                content: content,
+                speaker: name,
+                speaker_id: id,
+                pics: pic
+            };
+            req.models.message_session.create({
+                day: new Date(),
+                sick_id: id,
+                doctor_id: doctor_id,
+                message: msg
+            }, function (err, item) {
+                msg.session_id = item.id;
+                req.models.message.create(msg, function (err) {
                     if (err) {
                         res.redirect(source_url[type] + "?err=2");
                     } else {
                         res.redirect(target_url[type]);
                     }
                 });
-            }
+            });
+        } else {
+            req.models.message.create({
+                session_id: session_id,
+                day: new Date(),
+                title: title,
+                content: content,
+                speaker: name,
+                speaker_id: id,
+                pics: pic
+            }, function (err, item) {
+                if (err) {
+                    res.redirect(source_url[type] + "?err=2");
+                } else {
+                    res.redirect(target_url[type]);
+                }
+            });
         }
-    });
+    }
+
 
 });
 
