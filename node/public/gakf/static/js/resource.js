6bf14b02b3bb8411703235235b8aed6d66d673ca
Alice <5541037@qq.com>
2015-09-05 10:35:07 +0800

    fix liangbiao and date

--- node/public/gakf/static/js/resource.js
+++ node/public/gakf/static/js/resource.js
@@ -123,7 +123,7 @@ RC['setItem'] = function (d){
                         contains = con;
                    }
                    
-                   $(contains).parent().parent().trigger("tap");
+                   $(contains).parent().parent().trigger("click");
                 }
             }else if(temp.type == "input"){
                 for(var j = 0;j<temp.keys.length;j++){
